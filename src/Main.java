8002bf7039eb331a3b4cf0b01b461c6932350588
Roman Milkov <mir3227@mail.ru>
2023-12-22 00:19:21 +0500

    README adjustment

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import static java.sql.DriverManager.println;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1);
+        try {
+            int c = calc.devide.apply(a, b);
+            calc.println.accept(c);
+        } catch (ArithmeticException e) {
+            System.out.println("Попытка выполнить недопустимую арифметическую операцию");
+        }
+
+
+    }
+}
